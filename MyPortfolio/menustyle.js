7b1793eea90ff007e0691d5ed8d27f9df14a70e4
Cocoy Maghanoy <rmcocoy@gmail.com>
2022-12-31 19:08:51 +0800

    first commit

---
+++ MyPortfolio/menustyle.js
@@ -0,0 +1,11 @@
+
+
+const bars = document.getElementById('bars')
+const navmenu = document.getElementById('navmenu')
+
+bars.addEventListener('click', function() {
+  navmenu.classList.toggle('active')
+  console.log('test')
+})
+
+
